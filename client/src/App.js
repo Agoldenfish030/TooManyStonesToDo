56b76c6981c832bf88cf92809ff2d1ba6f796752
aGoldenFish030 <rain20854@gmail.com>
2025-12-19 21:53:58 +0800

    try to authorize successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,40 @@
 import './App.css';
+import { useEffect } from 'react';
+
+const authenticationSuccess = function() {
+  console.log('Successful authentication');
+};
+
+const authenticationFailure = function() {
+  console.log('Failed authentication');
+};
+
+const handleLogin = ()=>{
+  window.Trello.authorize({
+    type: 'popup',
+    name: 'TooMuchStonesToDo_power-up',
+    scope: {
+      read: 'true',
+      write: 'true' },
+    expiration: 'never',
+    success: authenticationSuccess,
+    error: authenticationFailure
+  });
+}
 
 function App() {
+  useEffect(()=>{
+    const script = document.createElement('script');
+    script.src = `https://trello.com/1/client.js?key=${process.env.MY_API_KEY}`;
+    document.head.appendChild(script);
+  });
+  
   return (
     <div className="App">
+      <h1>Welcome</h1>
+      <button onClick={handleLogin}>
+        點我使用該死的授權
+      </button>
     </div>
   );
 }
