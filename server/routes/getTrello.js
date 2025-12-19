85b606b09a20755a04bb8fbf692f61a2753783dd
aGoldenFish030 <rain20854@gmail.com>
2025-12-19 22:46:38 +0800

    try authorization again2

--- server/routes/getTrello.js
+++ server/routes/getTrello.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 router.get("/", (req, res)=>{
+    console.log("yes");
     const link = process.env.AUTHORIZE_URL;
     link.replace('<KEYWORD>', process.env.MY_API_KEY);
     if(!link) res.status(500).json({ message: "LINK LOST" });
