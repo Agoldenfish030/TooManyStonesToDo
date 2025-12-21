774ce0d46e34b371bde2ba322948f05149792c78
Agoldenfish030 <rain20854@gmail.com>
2025-12-21 21:22:25 +0800

    try

--- server/routes/getTrello/getWebhook.js
+++ server/routes/getTrello/getWebhook.js
@@ -1,5 +1,5 @@
-const fetch = require('node-fetch');
-const dotenv = require('dotenv');
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const getWebhook = async({ userState })=>{
