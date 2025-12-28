edb616ed54d6f1ee4f7543c6b99708e4edbc335b
Agoldenfish030 <rain20854@gmail.com>
2025-12-28 23:08:20 +0800

    try listenWebhook

--- server/routes/getTrello/listenWebhook.js
+++ server/routes/getTrello/listenWebhook.js
@@ -11,17 +11,17 @@ router.post("/", async(req, res)=>{
     try{
         //check whether the request is from trello or not
         if(verifyWebhook(req, 'https://toomuchstonestodo.onrender.com/listenWebhook')){
-            const response = req.body.json();
+            const response = req.body;
             const action = response.action;
             const model = response.model;
             const webhook = response.webhook;
 
-            const boardID = model.id;
-            const found = await User.findOne({ "boardWebhook.id": boardID });
+            const found = await User.findOne({ "boardWebhook.id": "webhook.id" });
             if(!found){
                 console.error("有未刪除的webhook！id：", webhook.id);
                 return res.status(410);
             }
+            const boardID = model.id;
             ///* for check content of the data
             console.log(
                 "正確收到webhook request！\n" +
